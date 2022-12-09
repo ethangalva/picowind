ea78fa6c9efdcdb6e185100e8286ee10d06dde35
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-12-09 10:33:24 -0800

    feat: footer mobile

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Header from './components/header'
+import Footer from './components/footer'
+import Landing from './components/landing'
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
 
       <main>
         <Header />
+        <Landing />
+        <Footer />
       </main>
     </div>
   )
